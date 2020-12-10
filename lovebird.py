cb2891775b44cce3b3240683bfa79aa7231081cc
Khairul Azhar <67180998+Khairul989@users.noreply.github.com>
2020-12-10 14:24:12 +0800

    Small update

--- lovebird.py
+++ lovebird.py
@@ -1,11 +1,9 @@
 from twilio.rest import Client 
  
-account_sid = 'AC03a4b4ec11f205dfb6a16f17acf84233' 
-auth_token = 'e3ad49b6bf2547caefe354e9630050f0' 
 client = Client(account_sid, auth_token) 
 def sendLove():
     message = client.messages.create( 
-                                  from_='whatsapp:+14155238886',  
+                                  from_='whatsapp:+',  
                                   body='Assalamualaikum saya fyp, nak tanya fyp dah buat ke?',      
                                   to='whatsapp:+601156474458' 
                               ) 
